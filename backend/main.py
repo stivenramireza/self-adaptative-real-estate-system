bd9dd178bebbf2bfb12ff9cdaac81e4319675c09
nicolaslp085 <92638829+nicolaslp085@users.noreply.github.com>
2022-05-29 18:11:18 -0500

    backnd

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,31 @@
-def main() -> None:
-    pass
+from flask import Flask
+from flask import jsonify
+from firebase_admin import credentials, firestore, initialize_app
+import firebase_admin
+
+def create_app():
+    app = Flask(__name__)
+    return app
+
+app = create_app()
+
+# Initialize Firestore DB
+cred = credentials.Certificate('key.json')
+firebase_admin.initialize_app(cred)
+#default_app = initialize_app(cred)
+db = firestore.client()
+inmuebles = db.collection('Inmuebles')
+
+
+@app.route('/api/v1/control/', methods=['POST'])    
+def create_user():
+    
+    all_inmuebles = [doc.to_dict() for doc in inmuebles.stream()]
+    return jsonify(all_inmuebles), 200
+
+#    response = {'message': 'successPOST'}
+#    return jsonify(response)
 
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
