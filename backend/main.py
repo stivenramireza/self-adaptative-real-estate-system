dc8141c5d709b987d733d0935f235d3519dbe73d
Stiven Ramírez Arango <stivenramireza@gmail.com>
2022-05-29 19:03:41 -0500

    Endpoint finished

--- backend/main.py
+++ backend/main.py
@@ -1,31 +1,45 @@
-from flask import Flask
-from flask import jsonify
-from firebase_admin import credentials, firestore, initialize_app
 import firebase_admin
+from firebase_admin import credentials, firestore
+from flask import Flask, request
+from flask import jsonify
+
 
 def create_app():
     app = Flask(__name__)
     return app
+
 
 app = create_app()
 
 # Initialize Firestore DB
 cred = credentials.Certificate('key.json')
 firebase_admin.initialize_app(cred)
-#default_app = initialize_app(cred)
+# default_app = initialize_app(cred)
 db = firestore.client()
-inmuebles = db.collection('Inmuebles')
+properties = db.collection('Inmuebles')
 
 
-@app.route('/api/v1/control/', methods=['POST'])    
+@app.route('/api/v1/control/', methods=['POST'])
 def create_user():
-    
-    all_inmuebles = [doc.to_dict() for doc in inmuebles.stream()]
-    return jsonify(all_inmuebles), 200
+    body = request.json
+    property_id = body.get('property_id')
 
-#    response = {'message': 'successPOST'}
-#    return jsonify(response)
+    property = properties.document(property_id).get().to_dict()
+    visits = property.get('Visits')
+
+    property.update({'Visits': visits + 1})
+
+    properties.document(property_id).update(property)
+
+    property = properties.document(property_id).get().to_dict()
+
+    if property.get('Visits') % 5 == 0:
+        price = property.get('Price')
+        property.update({'Price': price + 100_000})
+        properties.document(property_id).update(property)
+
+    return jsonify(property), 200
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
